e78631c41ef7aba6761bdddb6e827db55439f2d7
Volodymyr <vovikdrg@gmail.com>
2015-02-08 23:28:37 +0200

    plot 2

--- plot2.R
+++ plot2.R
@@ -3,11 +3,9 @@ data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 data$Global_active_power <- as.numeric(data$Global_active_power)
 dataForPlot <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-
-png("plot1.png", width=480, height=480)
-hist(dataForPlot$Global_active_power, 
-     main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", 
-     ylab="Freqency", 
-     col="Red")
+datetime <- as.POSIXct(paste(dataForPlot$Date, dataForPlot$Time))
+png("plot2.png", width=480, height=480)
+plot(dataForPlot$Global_active_power~datetime, type="l", 
+     ylab="Global Active Power (kilowatts)", 
+     xlab="")
 dev.off()
